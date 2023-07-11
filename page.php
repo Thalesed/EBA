b06a0e9f8c6171b5d954faa7e79326e86d015e1e
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-07-11 15:04:15 -0300

    Delete page.php

--- page.php
+++
@@ -1,7 +0,0 @@
-
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
-<?php get_header();?>
-<h1>Teste</h1>
-
-<p> Essa é uma página de Teste</p>
-<?php get_footer();?>
