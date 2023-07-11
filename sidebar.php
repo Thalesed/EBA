6c3778669e0c408886af4494de51fc7c1522528f
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-07-11 15:04:00 -0300

    Delete sidebar.php

--- sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside id="sidebar" class="sidebar">
-  <?php if (is_active_sidebar('sidebar-1')) : ?>
-    <?php dynamic_sidebar('sidebar-1'); ?>
-  <?php else : ?>
-    <!-- Conteúdo padrão da barra lateral -->
-  <?php endif; ?>
-</aside>
-
