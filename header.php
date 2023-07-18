755d3ea448e2357f223fe8c0519366b4a80212e7
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-07-18 14:31:20 -0300

    Add files via upload

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
             <meta charset="<?php bloginfo('charset'); ?>">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	    <?php wp_head(); ?>
+	    <?php //wp_head(); ?>
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
         </head>
 <body>
--- page.php
+++ page.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
-<div class="container pt-5">
+<div>
         <div class="row">
-                <div class="col-sm-8">
+                <main class="col-sm-8">
                         <?php  if( have_posts() ) : while( have_posts() ) : the_post(); ?>
                                 <h1><?php the_title(); ?></h1>
                                 <div>
                                         <?php the_content(); ?>
                                 </div>
                         <?php endwhile; endif; ?>
-                </div>
+                </main>
                 <div class="sidebar">
                         <?php
 
